363cceab987b6fa097c88f0bf4a063c0125a0bcb
adityaraaj123478-droid <adityaraaj123478@gmail.com>
2026-03-23 14:48:44 +0530

    Aditya Raj Added UC2 Room Initialization

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,59 @@
+abstract class Room {
+    String type;
+    int beds;
+    double price;
+
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: " + price);
+    }
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 2000);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+}
+
 public class BookMyStayApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to BookMyStay App");
-        System.out.println("Book your stay easily!");
+
+        Room single = new SingleRoom();
+        Room doub = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("=== Room Details ===");
+
+        single.display();
+        System.out.println("Available: " + singleAvailable);
+
+        doub.display();
+        System.out.println("Available: " + doubleAvailable);
+
+        suite.display();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
